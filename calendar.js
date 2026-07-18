ff2532ac968b375aa4555b1f01d1e0238b653df8
Tildawah04 <tildawah@chalmers.se>
2026-07-18 15:24:57 +0200

    Create calendar.js

---
+++ calendar.js
@@ -0,0 +1,121 @@
+import {
+
+auth,
+
+db
+
+}
+
+from "./firebase.js";
+
+
+
+import {
+
+collection,
+
+addDoc,
+
+getDocs
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+
+
+
+window.addWorkout=function(){
+
+
+let user=auth.currentUser;
+
+
+
+addDoc(
+
+collection(db,"workouts"),
+
+{
+
+
+user:user.email,
+
+date:date.value,
+
+training:training.value
+
+
+}
+
+
+)
+
+.then(load);
+
+
+}
+
+
+
+
+
+async function load(){
+
+
+list.innerHTML="";
+
+
+let data=
+
+await getDocs(
+
+collection(db,"workouts")
+
+);
+
+
+
+data.forEach(x=>{
+
+
+let w=x.data();
+
+
+list.innerHTML +=
+
+`
+
+<p>
+
+📅 ${w.date}
+
+<br>
+
+👤 ${w.user}
+
+<br>
+
+🏃 ${w.training}
+
+</p>
+
+
+`;
+
+
+});
+
+
+}
+
+
+
+auth.onAuthStateChanged(()=>{
+
+load();
+
+});
